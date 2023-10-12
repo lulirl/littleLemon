be4f54cf0d0d2ba63966fed9f132a0424718f5f0
Ignacio Padilla <ignaciop@nicasource.com>
2023-10-12 16:22:38 -0300

    initial commit

---
+++ LittleLemonFinal/LittleLemonFinal/LittleLemonFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonFinalApp.swift
+//  LittleLemonFinal
+//
+//  Created by Ignacio Padilla on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
